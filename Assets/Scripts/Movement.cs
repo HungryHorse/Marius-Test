5473d49e03baaff882f72fe7691f63f634789d79
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-10-05 15:58:04 +0100

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed;
+
+    private Transform cubeTransform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cubeTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        // cubeTransform.position += cubeTransform.forward * speed * Time.deltaTime;	
+	}
+    
+}
